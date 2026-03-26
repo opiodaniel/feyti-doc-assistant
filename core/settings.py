60b11884880c67ab5830f7c9198f75ffa7f8ceb3
Opio Daniel <danielopio540@gmail.com>
2026-03-26 11:03:39 +0300

    Config: Added djangorestframework and CORS headers configuration

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = True
